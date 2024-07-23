63e01658e1d4cca7eb4188e1c4f3b84035c95538
Ryan Styles <ryan.c.styles@gmail.com>
2024-07-23 15:58:42 -0500

    chore: Add Pokemon search endpoint to API

--- api/api.py
+++ api/api.py
@@ -1,11 +1,16 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from repositories.pokemon_repository import PokemonRepository
 from db.pokemon_db import PokemonDB
 from models.pokemon import Pokemon
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 db = PokemonDB()
+repo = PokemonRepository()
 
 @app.route('/pokemon', methods=['POST'], strict_slashes=False)
 def catch_pokemon():
@@ -72,6 +77,28 @@ def release_pokemon(capture_id):
     """
     db.release_pokemon(capture_id)
     return "Pokemon released successfully"
+
+@app.route('/pokemon/search/<name>', methods=['GET'], strict_slashes=False)
+def search_pokemon(name):
+    """
+    Searches for a Pokemon by name.
+
+    Args:
+        name (str): The name of the Pokemon to search for.
+
+    Returns:
+        Pokemon: The Pokemon object if found, otherwise None.
+    """
+    my_pokemon = repo.get_pokemon(name)
+    return jsonify({
+        "capture_id": my_pokemon.capture_id,
+        "name": my_pokemon.name,
+        "type": my_pokemon.type,  # Deserialize JSON string to list
+        "abilities": my_pokemon.abilities,  # Deserialize JSON string to list
+        "height": my_pokemon.height,
+        "weight": my_pokemon.weight,
+        "image": my_pokemon.image
+    })
 
 if __name__ == '__main__':
     app.run(port=5000)
